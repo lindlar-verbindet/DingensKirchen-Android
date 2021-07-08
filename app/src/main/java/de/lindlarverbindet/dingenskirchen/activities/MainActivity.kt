eee66a75ef4561e9b4d4539877dc63dec6262a29
pixelskull <pascal.schoenthier@th-koeln.de>
2021-07-08 17:57:22 +0200

    Added layout for VillageActivity to match layout

--- app/src/main/java/de/lindlarverbindet/dingenskirchen/activities/MainActivity.kt
+++ app/src/main/java/de/lindlarverbindet/dingenskirchen/activities/MainActivity.kt
@@ -9,16 +9,25 @@ import de.lindlarverbindet.dingenskirchen.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mobilWidget: View
+    private lateinit var villageWidget: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mobilWidget = findViewById(R.id.main_mobil_widget)
+        villageWidget = findViewById(R.id.main_village_widget)
 
         mobilWidget.setOnClickListener {
             runOnUiThread{
                 val intent = Intent(applicationContext, MapActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        villageWidget.setOnClickListener {
+            runOnUiThread {
+                val intent = Intent(applicationContext, VillageActivity::class.java)
                 startActivity(intent)
             }
         }
