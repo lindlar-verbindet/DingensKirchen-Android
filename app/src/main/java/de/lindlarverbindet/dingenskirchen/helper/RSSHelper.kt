9bd5b597808db49832c70111be459561bf7c047d
pixelskull <pascal.schoenthier@th-koeln.de>
2022-06-26 13:09:48 +0200

    Simplified Exception handling in RSSHelper to catch all Exceptions

--- app/src/main/java/de/lindlarverbindet/dingenskirchen/helper/RSSHelper.kt
+++ app/src/main/java/de/lindlarverbindet/dingenskirchen/helper/RSSHelper.kt
@@ -3,25 +3,19 @@ package de.lindlarverbindet.dingenskirchen.helper
 import de.lindlarverbindet.dingenskirchen.models.News
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
-import java.io.FileNotFoundException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
-import java.net.UnknownHostException
 import java.text.SimpleDateFormat
 import java.util.*
-import javax.net.ssl.SSLException
 
 class RSSHelper {
-
-    private val apiHelper = APIHelper()
 
     fun getRecentPosts(): List<News>? {
         val urlString = "https://lindlar.de/rss.html"
         val url = URL(urlString)
 
         val dateParser = SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z", Locale.US)
-//        val response = apiHelper.sendGetRequest(urlString)
         val http = url.openConnection() as HttpURLConnection
         http.doInput = true
         try {
@@ -67,11 +61,7 @@ class RSSHelper {
                 eventType = xpp.next()
             }
             return result.toList()
-        } catch (e: FileNotFoundException) {
-            return null
-        } catch (e: UnknownHostException) {
-            return null
-        } catch (e: SSLException) {
+        } catch (e: Exception) {
             return null
         }
     }
